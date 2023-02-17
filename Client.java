e7a51ed4bd4625ecc6e4f150dd96f2cab1eece77
surya-guruvu <74617272+surya-guruvu@users.noreply.github.com>
2023-02-17 21:02:03 +0530

    Multiple Client and Server Threads Implemented

--- Client.java
+++ Client.java
@@ -5,18 +5,52 @@ public class Client {
 
     //Intialize socket and input,output streams
 
-    private Socket            clientSocket = null;
-    private DataInputStream   input        = null;
-    private DataOutputStream  output       = null;
-    private BufferedReader    stdIn        = null;
+    private Socket            clientSendSocket  = null;
+    private Socket            clientRecvSocket  = null;
+    private DataInputStream   input_send        = null;
+    private DataOutputStream  output_send       = null;
+    private DataInputStream   input_recv        = null;
+    private DataOutputStream  output_recv       = null;
+    private BufferedReader    stdIn             = null;
 
-    public Client(String address,int port){
+
+    public Client(String address,int sendPort,int recvPort){
         try{
-            clientSocket = new Socket(address,port);
-            System.out.println("Connected");
-            input  = new DataInputStream(clientSocket.getInputStream());   //takes input from socket
-            output = new DataOutputStream(clientSocket.getOutputStream()); //sends output to socket
             stdIn  = new BufferedReader(new InputStreamReader(System.in)); //takes input from terminal
+                
+            System.out.println("Enter username:\n");
+
+            String username = "";
+            username = stdIn.readLine();
+
+            System.out.println("Registering to send\n");
+
+            clientSendSocket = new Socket(address,sendPort);
+
+            input_send  = new DataInputStream(clientSendSocket.getInputStream());   //takes input from socket
+            output_send = new DataOutputStream(clientSendSocket.getOutputStream()); //sends output to socket
+
+            output_send.writeUTF(username);
+
+
+            System.out.println("Registered to send\n");
+
+            System.out.println("Registering to recv\n");
+
+            clientRecvSocket = new Socket(address,recvPort);
+
+            input_recv  = new DataInputStream(clientRecvSocket.getInputStream());
+            output_recv = new DataOutputStream(clientRecvSocket.getOutputStream());
+
+            output_recv.writeUTF(username);
+
+            System.out.println("Registered to recv\n");
+
+            Thread serverThreadSend = new Thread(new ServerHandler(clientSendSocket,stdIn,0,input_send,output_send));
+            Thread serverThreadRecv = new Thread(new ServerHandler(clientRecvSocket,stdIn,1,input_recv,output_recv));
+
+            serverThreadSend.start();
+            serverThreadRecv.start();
 
         }
         catch (UnknownHostException u) {
@@ -27,31 +61,94 @@ public class Client {
             System.out.println(i);
             return;
         }
-        try{
+
+    }
+
+    private static class ServerHandler implements Runnable {
+        private Socket            clientSocket  = null;
+        private DataInputStream   input         = null;
+        private DataOutputStream  output        = null;
+        private BufferedReader    stdIn         = null;
+        int op                                  = 1; //0 for send, 1 for recv
+        String arr[]                            = {"send","recv"};
+
+        public ServerHandler(Socket socket,BufferedReader stdIn,int op,DataInputStream input,DataOutputStream output){
+            this.clientSocket = socket;
+            this.stdIn        = stdIn; 
+            this.op           = op;  
+            this.input        = input;
+            this.output       = output;
+        }
+        private void sendSocketHandler(){
+            
             //string to read the message from stdIn
             String userInput = "";
+            try{
+                while(true){
+                    while((userInput = stdIn.readLine())!=null){
+                        output.writeUTF(userInput);
+                    }
+                }
 
-            while((userInput = stdIn.readLine())!=null){
-                output.writeUTF(userInput);
-                System.out.println("echo: " + input.readUTF());
+            }
+            catch (IOException i) {
+                System.out.println(i);
+                return;
+            }
+
+            
+        }
+        
+        private void recvSocketHandler(){
+
+            
+
+        }
+        
+        public void run(){
+            try{
+                if(op==1){
+                    try{
+                        String inputLine;
+                        while(true){
+                            while ((inputLine = input.readUTF()) != null) {
+                                System.out.println(inputLine);
+                            }
+                        }
+
+
+                    }
+
+                    catch (IOException i) {
+                        System.out.println(i);
+                        return;
+                    }
+                    
+                }
+                else{
+                    while(true){
+                        sendSocketHandler();
+                    }
+                }
+                
+                    
+                
+            }
+
+            finally {
+                try {
+                    input.close();
+                    output.close();
+                    clientSocket.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
             }
         }
-        catch (IOException i) {
-            System.out.println(i);
-            return;
-        }
 
-        //close the connection
-        try {
-            input.close();
-            output.close();
-            clientSocket.close();
-        }
-        catch (IOException i) {
-            System.out.println(i);
-        }
+
     }
     public static void main(String[] args) throws IOException {
-        Client client = new Client("127.0.0.1", 5000);
+        Client client = new Client("127.0.0.1", 5000,8000);
     }
 }
--- Server.java
+++ Server.java
@@ -1,23 +1,45 @@
 import java.net.*;
 import java.io.*;
+import java.util.*;
 
 public class Server{
-    private Socket          socket   = null;
-    private ServerSocket    server   = null;
+    private Socket          sendSocket   = null;
+    private Socket          recvSocket   = null;
+    private ServerSocket    serverSend   = null;
+    private ServerSocket    serverRecv   = null;
+    private BufferedReader  stdIn        = null;
 
-    public Server(int port){
+    HashMap<String,ClientHandler> mapSend = null;
+    HashMap<String,ClientHandler> mapRecv = null;
+
+    public Server(int sendPort,int recvPort){
         try{
             //server starts
-            server = new ServerSocket(port);
+            serverSend = new ServerSocket(sendPort);
+            serverRecv = new ServerSocket(recvPort);
+
+            mapSend = new HashMap<String,ClientHandler>();
+            mapRecv = new HashMap<String,ClientHandler>();
 
             System.out.println("Server Started");
 
             System.out.println("Waiting for a Client .....");
 
+            stdIn  = new BufferedReader(new InputStreamReader(System.in));
+
             while(true){
-                socket = server.accept();
-                Thread clientThread = new Thread(new ClientHandler(socket));
-                clientThread.start();
+                sendSocket = serverSend.accept();
+                recvSocket = serverRecv.accept();
+
+
+                ClientHandler ClientHandlerSend = new ClientHandler(sendSocket,0,stdIn);
+                ClientHandler ClientHandlerRecv = new ClientHandler(recvSocket,1,stdIn);
+
+                Thread clientThreadSend = new Thread(ClientHandlerSend);
+                Thread clientThreadRecv = new Thread(ClientHandlerRecv);
+
+                clientThreadSend.start();
+                clientThreadRecv.start();
             }
         }
         catch(IOException i)
@@ -26,24 +48,52 @@ public class Server{
         }
     }
     private static class ClientHandler implements Runnable {
-        private Socket clientSocket;
-        private DataInputStream in;
-        private DataOutputStream out;
+        private Socket clientSocket        = null;
+        private DataInputStream in         = null;
+        private DataOutputStream out       = null;
+        private BufferedReader stdIn       = null;
+        private int op                     = 0;
+        private String arr[]               = {"send","recv"};
 
-        public ClientHandler(Socket socket) {
+        public ClientHandler(Socket socket,int op,BufferedReader stdIn) {
             this.clientSocket = socket;
+            this.op           = op;
+            this.stdIn        = stdIn;
         }
 
         public void run() {
             try {
-                in = new DataInputStream(clientSocket.getInputStream());
+
+                System.out.println("Registering to "+arr[op]);
+
+                in  = new DataInputStream(clientSocket.getInputStream());
                 out = new DataOutputStream(clientSocket.getOutputStream());
 
-                String inputLine, outputLine;
-                while ((inputLine = in.readUTF()) != null) {
-                    outputLine = "Server: " + inputLine;
-                    out.writeUTF(outputLine);
+                String username = in.readUTF();
+
+                System.out.println("Username: "+username);
+
+                System.out.println("Registered to "+arr[op]);
+
+                if(op==0){
+                    String inputLine;
+                    while(true){
+                        while ((inputLine = in.readUTF()) != null) {
+                            System.out.println(inputLine);
+                        }
+                    }
                 }
+                else{
+                    String outputLine;
+                    while(true){
+                        while((outputLine = stdIn.readLine())!=null){
+                            out.writeUTF(outputLine);
+                        }
+                    }
+                }
+
+
+
             } catch (IOException e) {
                 e.printStackTrace();
             } finally {
@@ -58,6 +108,6 @@ public class Server{
         }
     }
     public static void main(String[] args) throws IOException{
-        Server server = new Server(5000);
+        Server server = new Server(5000,8000);
     }
 }
